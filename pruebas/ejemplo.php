5e7dd02209bfcc4c697e0d51c95b0831ad1b597c
JohnMacias116 <67765312+JohnMacias116@users.noreply.github.com>
2020-07-16 21:27:08 -0500

    Api
    Api agregada

---
+++ pruebas/ejemplo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">	
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Ejemplo de Github</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+	</head>
+	<body>
+		<header> Ejemplo </header>
+		
+			 <form action="procesa.php" method="POST">
+				<input type="text" name="nombre" placeholder="Nombre" >
+				<input type="text" name="apellido" placeholder="Apellido" >
+				      <div class="g-recaptcha" data-sitekey="6Lf_BrEZAAAAABN9WZAveCYTOYxiSry43P3Hkolp"></div>
+      <input type="submit" value="Submit">
+
+
+			</form>
+		
+		<a href="next.php"> Next </a> 
+	</body>
+	
+</html>
---
+++ pruebas/next.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>-API mapa</title>
+	<style>
+		#map{
+			width: 1000px;
+			height: 1000px;
+		}
+	</style>
+</head>
+<body>
+	<div id="map"></div>
+	<script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAl7d-PowaMfupOEqTpynWb6_SmYWtOrB8&callback=initMap">
+</script>
+<script src="mains.js"></script>
+<script src="localizacion.js"></script>
+
+</body>
+</html>
